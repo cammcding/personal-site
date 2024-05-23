e0a1f6bfc91aea178672a4c25f1ed8c686580a20
Cam Dingman <crmdingm@uwaterloo.ca>
2024-05-23 18:04:26 -0400

    home page title progress

--- impl/src/routes/HomePage.jsx
+++ impl/src/routes/HomePage.jsx
@@ -8,6 +8,11 @@ export default function HomePage() {
             <div className="home-page-blocks-container" id="blocks">
                 <AnimatedBlocks />
             </div>
+            <div className="home-page-title-container">
+                <span className="home-page-title" id="title-make">MAKE</span>
+                <span className="home-page-title" id="title-coding"> CODING </span>
+                <span className="home-page-title" id="title-fun">FUN</span>
+            </div>
         </div>
     );
 }
