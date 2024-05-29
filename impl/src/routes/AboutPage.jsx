ae5607bbc8d4f356c48063476b7513706d4d88b9
Cam Dingman <crmdingm@uwaterloo.ca>
2024-05-29 09:57:53 -0400

    added external links to about page

--- impl/src/routes/AboutPage.jsx
+++ impl/src/routes/AboutPage.jsx
@@ -1,5 +1,7 @@
 import Background from "../assets/banff-mountains.jpg";
 import Portrait from "../assets/hiking-portrait.jpg";
+import GithubLogo from "../assets/github-logo.svg";
+import EmailLogo from "../assets/email-logo.svg";
 import MultiColouredText from "../components/MultiColouredText";
 
 export default function AboutPage() {
@@ -13,6 +15,11 @@ export default function AboutPage() {
 
     const aboutMe2 = "Hopefully something here inspired you! And don’t " +
     "forget to go outside, there’s lots to see out there… ";
+    
+    // Breaking email up like this adds extra protect from spam bots scraping
+    //   the page.
+    const eml = 'ca' + 'm.' + 'mc' + 'di' + 'ng' + '@g' + 'ma' + 'il' + '.c' +
+    'om';
 
     return(
         <div 
@@ -22,12 +29,22 @@ export default function AboutPage() {
                 <img className="about-page-portrait" src={Portrait}/>
             </div>
             <div className="about-page-info-container">
-                <h1>Hi, I'm Cam!</h1>
-                <p>{aboutMe1}</p>
-                <br></br>
-                <p>{aboutMe2}</p>
-                <br></br>
-                <MultiColouredText text={["Happy", "Coding!"]} />
+                <div className="about-page-info">
+                    <h1>Hi, I'm Cam!</h1>
+                    <p>{aboutMe1}</p>
+                    <br></br>
+                    <p>{aboutMe2}</p>
+                    <br></br>
+                    <MultiColouredText text={["Happy", "Coding!"]} />
+                </div>
+                <div className="about-page-links">
+                    <a href="https://github.com/cammcding">
+                        <img className="about-page-link-logo" src={GithubLogo}/>
+                    </a>
+                    <a href={"mai" + "lto:" + eml}>
+                        <img className="about-page-link-logo" src={EmailLogo}/>
+                    </a>
+                </div>
             </div>
         </div>
     );
