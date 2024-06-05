c78d0e6de19c6331249ca9b1457d23204541a103
Cam Dingman <crmdingm@uwaterloo.ca>
2024-06-05 12:02:25 -0400

    projects page styling

--- impl/src/components/ProjectCard.jsx
+++ impl/src/components/ProjectCard.jsx
@@ -3,24 +3,30 @@ import PropTypes from "prop-types";
 export default function ProjectCard(props) {
 
     const langListItems = props.langs.map((lang) => 
-        <li className="language-used" key={lang}>{lang}</li>
+        <li className="project-card-lang-used" key={lang}>{lang}</li>
     );
 
     return(
         <div className="project-card">
-            <a className="project-card-open-link" href={props.link}>
-                <h2 className="project-card-title">{props.name}</h2>
-            </a>
-            <a className="project-card-open-link" href={props.link}>
+            <div className="project-card-title-container">
+                <a className="project-card-open-link" href={props.link}>
+                    <h2 className="project-card-title">{props.name}</h2>
+                </a>
+            </div>
+            <div className="project-card-img-container">
                 <img className="project-card-img" src={props.imgSource}></img>
-            </a>
-            <p className="project-card-desc">{props.desc}</p>
-            <h3 className="project-card-lang-title">
-                Languages/Tools/Frameworks Used:
-            </h3>
-            <ul className="project-card-languages-used-list">
-                {langListItems}
-            </ul>
+            </div>
+            <div className="project-card-desc-container">
+                <p className="project-card-desc">{props.desc}</p>
+            </div>
+            <div className="project-card-langs-container">
+                <h3 className="project-card-lang-title">
+                    Languages/Tools/Frameworks Used:
+                </h3>
+                <ul className="project-card-langs-list">
+                    {langListItems}
+                </ul>
+            </div>
         </div>
     );
 }
--- impl/src/routes/ProjectsPage.jsx
+++ impl/src/routes/ProjectsPage.jsx
@@ -5,18 +5,24 @@ export default function ProjectsPage() {
 
     return(
         <div className="projects-page-container"> 
+            <div className="projects-page-buffer"></div>
             <div className="projects-page-top-text-container">
                 <h1 className="projects-page-title">
                     Personal Projects
                 </h1>
                 <p className="projects-page-info">
-                    The following are previews of my personal programming projects.
-                    The best project is one that you will finish! So with that in mind,
-                    I prioritized my interests when developing these projects. 
+                    The following are previews of both my personal programming 
+                    projects and interesting school projects. The best project 
+                    is one that you will finish! So with that in mind,
+                    I prioritized my interests when developing these. 
                     Hope you enjoy!
                 </p>
+                
             </div>
-            <div className="projects-page-preview-container">
+            <h3 className="projects-page-link-prompt">
+                Click on a project title for its external link 
+            </h3>
+            <div className="projects-page-project-cards-container">
                 <Projects />
             </div>
             <div className="projects-page-more-coming-text">
